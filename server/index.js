09f9e2bbbe8b683b58deae29c7c743285ff2eb88
john <wzhen0930@163.com>
2019-07-03 22:56:51 +0800

    [WN] 完成ssr初始框架

--- server/index.js
+++ server/index.js
@@ -5,32 +5,66 @@ const fs = require('fs')
 const path = require('path')
 
 const app = http.createServer((req, res) => {
+    if (/\/static\//.test(req.url)){ // 如果是获取静态资源的
+        const pathStr = path.join(__dirname, '..', 'build', req.url)
+        const ext = path.extname(pathStr).replace('.','')
+        const MIME_MAP = {
+            js:'text/javascript',
+            css:'text/css',
+        }
+        if (Object.keys(MIME_MAP).includes(ext)){ // 给js和css文件设content-type头
+            res.setHeader('Content-Type',MIME_MAP[ext])
+        }
+        return fs.createReadStream(pathStr).pipe(res)
+    }
     res.end(htmlTemplate(ReactDOMServer.renderToString(<App></App>)))
 })
 app.listen(1314)
 
-const arrJs = []
-const arrCss = []
-function getPath(root) {
-    const isDirectory = fs.statSync(root).isDirectory()
-    if (isDirectory) {
-        fs.readdirSync(root).forEach(i => {
-            getPath(path.resolve(root, i))
-        })
-    } else {
-        const extName = path.extname(root)
-        if (extName === '.js') {
-            arrJs.push(root)
-        } else if (extName === '.css') {
-            arrCss.push(root)
+class StaticResource {
+    arrJs = []
+    arrCss = []
+    exp = new RegExp(path.resolve(__dirname, '..', 'build').replace(/\//g,'\\/') + '(.*$)')
+    constructor(){
+        this.getPath(path.resolve(__dirname, '..', 'build', 'static'))
+    }
+    getPath(root) {
+        const isDirectory = fs.statSync(root).isDirectory()
+        if (isDirectory) {
+            fs.readdirSync(root).forEach(i => {
+                this.getPath(path.resolve(root, i))
+            })
+        } else {
+            const extName = path.extname(root)
+            if (extName === '.js') {
+                this.arrJs.push(root)
+            } else if (extName === '.css') {
+                this.arrCss.push(root)
+            }
         }
     }
+    getCssStr(){ // 获取css的html字符串
+        return this.arrCss.map(i=>{
+            const relPath = i.match(this.exp)[1]
+            if (!relPath){
+                return ''
+            }else{
+                return `<link href="${relPath}" rel="stylesheet">`
+            }
+        }).join('\n')
+    }
+    getJsStr(){ // 获取js的html字符串
+        return this.arrJs.map(i=>{
+            const relPath = i.match(this.exp)[1]
+            if (!relPath){
+                return ''
+            }else{
+               return `<script src="${relPath}"></script>`
+            }
+        }).join('\n')
+    }
 }
-
-getPath(path.resolve(__dirname, '..', 'build', 'static'))
-
-console.log(arrJs)
-console.log(arrCss)
+const sr = new StaticResource() // 实例化资源对象
 
 function htmlTemplate(reactDom) {
     return `
@@ -39,11 +73,12 @@ function htmlTemplate(reactDom) {
         <head>
             <meta charset="utf-8">
             <title>React SSR</title>
+            ${sr.getCssStr()}
         </head>
         
         <body>
-            <div id="app">${ reactDom}</div>
-            <script src="./app.bundle.js"></script>
+            <div id="root">${ reactDom}</div>
+            ${sr.getJsStr()}
         </body>
         </html>
     `;
@@ -51,7 +86,7 @@ function htmlTemplate(reactDom) {
 class App extends React.Component {
     render() {
         return (
-            <div>aaa</div>
+            <div>aa</div>
         )
     }
 }
